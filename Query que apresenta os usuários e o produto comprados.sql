c16a182c4d7fa559d0ed230d30a74653054607f5
Luis.Carvalho <dtr.luis18@gmail.com>
2025-09-20 23:42:26 -0300

    Update Query que apresenta os usuários e o produto comprados.sql

--- Query que apresenta os usuários e o produto comprados.sql
+++ Query que apresenta os usuários e o produto comprados.sql
@@ -7,3 +7,13 @@ JOIN
     compras c ON u.id = c.id_usuario
 JOIN
     produtos p ON c.id_produto = p.id;
+
+--como funciona
+
+--SELECT u.nome, p.nome: Seleciona a coluna nome da tabela usuarios (apelidada de u) e a coluna nome da tabela produtos (apelidada de p).
+
+--FROM usuarios u: Começa a busca a partir da tabela de usuarios.
+
+--JOIN compras c ON u.id = c.id_usuario: Conecta a tabela usuarios com a tabela compras onde o id do usuário é o mesmo em ambas.
+
+--JOIN produtos p ON c.id_produto = p.id: Conecta o resultado anterior com a tabela produtos onde o id do produto é o mesmo.
