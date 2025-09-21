83d83869354a2e527f085e6126d657d510d635d6
Luis.Carvalho <dtr.luis18@gmail.com>
2025-09-20 23:43:13 -0300

    Update Query que retorna o código dos usuários na faixa etária "Entre 51 a 60 anos".sql

--- Query que retorna o código dos usuários na faixa etária "Entre 51 a 60 anos".sql
+++ Query que retorna o código dos usuários na faixa etária "Entre 51 a 60 anos".sql
@@ -4,3 +4,12 @@ FROM
     usuarios
 WHERE
     idade BETWEEN 51 AND 60;
+
+--Como funciona:
+
+--SELECT id AS codigo_usuario: Seleciona a coluna id da tabela usuarios e a renomeia para codigo_usuario no resultado.
+
+--FROM usuarios: Especifica que a busca será na tabela usuarios.
+
+--WHERE idade BETWEEN 51 AND 60: Filtra os resultados para incluir apenas os registros onde o valor da coluna idade está entre 51 e 60 (incluindo os dois números).
+
